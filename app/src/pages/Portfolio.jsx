c15c78a9bf39d1047aa7cae9df14d011a0da78ca
Celine Ross <celineross@gmail.com>
2020-12-11 22:47:09 -0600

    organized Portfolio page added styling and more images

--- app/src/pages/Portfolio.jsx
+++ app/src/pages/Portfolio.jsx
@@ -5,6 +5,7 @@ import audiofiler from "../assets/images/audiofiler.jpg";
 import artisthub from "../assets/images/artisthub.png";
 import emp from "../assets/images/emp.png";
 import resume from "../assets/images/resume.pdf";
+import budget from "../assets/images/budget.png"
 
 
 const styles = {
@@ -35,7 +36,7 @@ function Portfolio() {
                             <h1>Portfolio</h1>
                             <hr />
                             <p>View my resume <a style={styles.resumeStyle} href={resume}
-                                className="resume" target="_blank" rel="noreferrer">here.</a></p>
+                                className="resume" target="_blank" rel="noreferrer">here</a>.</p>
                             <p>
                                 Below are some small, sample projects I've worked on during my first few weeks in
                                 UTSA's coding bootcamp. Click the thumbnails to view their respective GitHub repositories.
@@ -45,51 +46,47 @@ function Portfolio() {
 
 
                         <div className="row">
-                            <div className="col-lg-6">
-                                <a href="https://github.com/celineross/celineross.github.io/tree/master/Homework3JS"
+                            <div className="col-lg-4">
+                                <a href="https://github.com/celineross/My-Budget"
                                     target="_blank" rel="noreferrer">
-                                    <img className="sites" src="images/final.png" alt="Password Generator" /></a>
+                                    <img className="sites" src={budget} alt="Budget Tracker" /></a>
                                 <p style={styles.pStyle}>Click <a style={styles.resumeStyle}
-                                    href="https://celineross.github.io/Homework3JS/" target="_blank" rel="noreferrer"> here </a> to view the app in action.
+                                    href="https://offline-budget-tracking.herokuapp.com/" target="_blank" rel="noreferrer"> here</a> to view the app in action.
                                 </p>
 
                                 <a href="https://github.com/celineross/Employee-Management-System" target="_blank" rel="noreferrer">
                                     <img className="sites" src={emp} alt="Employee Management System" /></a>
                                 <p style={styles.pStyle}>Click <a style={styles.resumeStyle}
-                                    href="https://github.com/celineross/Employee-Management-System/blob/main/gif/working.gif" target="_blank" rel="noreferrer"> here </a>
-                                to view the app in
-                                action.
+                                    href="https://github.com/celineross/Employee-Management-System/blob/main/gif/working.gif" target="_blank" rel="noreferrer"> here</a> to view the app in action.
+                                </p>
+                            </div>
+
+                            <div className="col-lg-4">
+                                <a href="https://github.com/celineross/React-Employee-Directory" target="_blank" rel="noreferrer">
+                                    <img className="sites" src="https://i.gyazo.com/25b586701cdac8a186004a30de1f9d39.jpg" alt="React Directory" /></a>
+                                <p style={styles.pStyle}>Click <a style={styles.resumeStyle}
+                                    href="https://celineross.github.io/React-Employee-Directory/" target="_blank" rel="noreferrer"> here</a> to view the app in action.
                                 </p>
 
-                                <a href="https://github.com/celineross/Note-Taker" target="_blank" rel="noreferrer">
-                                    <img className="sites" src="images/notetaker.png" alt="Note Taker" /></a>
+                                <a href="https://github.com/celineross/Night-In" target="_blank" rel="noreferrer">
+                                    <img className="sites" src={nightin} alt="Night In"></img></a>
                                 <p style={styles.pStyle}>Click <a style={styles.resumeStyle}
-                                    href="https://secure-castle-48565.herokuapp.com/" target="_blank" rel="noreferrer"> here </a>
-                                to view the app in
+                                    href="https://cascutter.github.io/Project-1-Night-In/" target="_blank" rel="noreferrer"> here</a> to view the app in
                                 action.
                                 </p>
                             </div>
 
-                            <div className="col-lg-6">
-                                <a href="https://github.com/celineross/Night-In" target="_blank" rel="noreferrer">
-                                    <img className="sites" src={nightin} alt="Night In"></img></a>
-                                <p style={styles.pStyle}>Click <a style={styles.resumeStyle}
-                                    href="https://cascutter.github.io/Project-1-Night-In/" target="_blank" rel="noreferrer"> here </a> to view the app in
-                                action.
-                                </p>
-
+                            <div className="col-lg-4">
                                 <a href="https://github.com/celineross/Project2" target="_blank" rel="noreferrer">
                                     <img className="sites" src={audiofiler} alt="AudioFiler" /></a>
                                 <p style={styles.pStyle}>Click <a style={styles.resumeStyle}
-                                    href="https://blooming-wildwood-71202.herokuapp.com/" target="_blank" rel="noreferrer"> here </a> to view the app in
+                                    href="https://blooming-wildwood-71202.herokuapp.com/" target="_blank" rel="noreferrer"> here</a> to view the app in
                                 action.
                                 </p>
 
-                                
                                 <a href="https://github.com/malex19889/ArtistHub" target="_blank" rel="noreferrer">
                                     <img className="sites" src={artisthub} alt="ArtistHub" /></a>
                                 <p style={styles.pStyle}>In Progress!</p>
-
                             </div>
 
                         </div>
@@ -98,7 +95,6 @@ function Portfolio() {
                 </div>
 
             </div>
-
         </div>
     );
 }
